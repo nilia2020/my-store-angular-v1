d36e08803f62a2f034303a7478e99d0485263acd
jorge niglia <jorge.niglia@gmail.com>
2023-01-10 21:57:02 -0300

    hasta eventos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-store'`, () => {
+  it(`should have as name 'jorge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-store');
+    expect(app.name).toEqual('jorge');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  position = 0;
+  name = 'jorge';
+  age = 50;
+  image = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  bkgr1 = "#ffaaaa"
+  person = {
+    name: 'Jorge',
+    age: 50,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+  plusAge() {
+    this.person.age += 1
+  }
+  minusAge() {
+    this.person.age -= 1
+  }
+
+onScroll(event: Event) {
+  const element = event.target as HTMLElement;
+  this.position = element.scrollTop;
+  if (this.position > 100) {
+    this.bkgr1 = "#dddd25"
+  } else {
+    this.bkgr1 = "#ff32aa"
+  }
+}
+changeName(event: Event) {
+  const element = event.target as HTMLInputElement;
+  this.person.name = element.value
+}
 }
